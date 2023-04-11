9b8c57d51c79c841d9664561a755f339ee9da750
meselghea <122159476+meselghea@users.noreply.github.com>
2023-04-11 21:57:44 +0700

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import login from "./Pages/login/login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import register from "./Pages/register/register";
 import Posts from "./Pages/Posts/Posts";
-import { Routes, Route } from "react-router-dom";
 import Post from "./Pages/Post/Post";
 
 const App = () => {
   return (
-    <>
+    <main className="app">
+      <login />
       <Routes>
-        <Route path="/" element={<Posts />} />
+      <Route path="/" element={<login />} />
+      <Route path="/register" element={<register />} />
+        <Route path="/posts" element={<Posts />} />
         <Route path="/post/:id" element={<Post />} />
       </Routes>
-    </>
+    </main>
   );
 };
 
